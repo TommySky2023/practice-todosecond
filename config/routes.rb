a479c6fe5f62900716af6a184ffd5a5b0d5d049d
DESKTOP-2STJP7I\user <tomoyuki.enomoto@gmail.com>
2024-09-18 07:50:37 +0900

    Profile actionを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  devise_for :users 
+  resources :users, only: [] do
+    resource :profile, only: [:edit, :update, :show]
+  end
+
+
+  root to: "boards#index"
+
+  resources :boards do
+    resources :tasks 
+      
+  end
+
+  resources :tasks, only: [] do
+    resources :comments, only: [:new, :destroy, :create]
+  end
+  
 end
+
+
